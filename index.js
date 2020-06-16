3d5f8b2e94ebdcf80d524dd99cbd259c2ae662d9
BkCece <BkCece@users.noreply.github.com>
2020-06-15 18:33:07 -0700

    postgreSQl test DB functioning

--- index.js
+++ index.js
@@ -2,9 +2,43 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+//For use with PostgreSQL database
+//From cmpt276 demo 5 
+const { Pool } = require('pg');
+var pool;
+pool = new Pool({
+    //connection to the database
+    //scheme://user:password@localhost/<localDBName>
+    connectionString: 'postgres://postgres:cmpt276@localhost/users'
+})
+
+var app = express();
+//express()
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+//File access through http
+app.use(express.static(path.join(__dirname, 'public')))
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => res.render('pages/index'))
+app.get('/database', (req, res) => {
+    //Create query to get all data from db
+    var getUsersQuery = 'SELECT * FROM userTable';
+
+    //Set return values for success and failure
+    pool.query(getUsersQuery, (error, result) => {
+        if (error)
+            res.end(error);
+        var results = { 'rows': result.rows }
+        res.render('pages/db', results);
+    });
+
+})
+
+
+
+
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
